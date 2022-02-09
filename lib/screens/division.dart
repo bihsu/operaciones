8da8f05ab31b6d1bc3751b2ca03f73087ed9c8b6
Adalidt <adan.limachi1@gmail.com>
2022-02-08 19:32:49 -0500

    operaciones completadas

--- lib/screens/division.dart
+++ lib/screens/division.dart
@@ -10,12 +10,96 @@ class Division extends StatelessWidget {
       appBar: AppBar(
         title: const Text('División'),
       ),
-      body: Center(
-        child: MaterialButton(
-          child: const Text('Regresar'),
-          color: Colors.orangeAccent,
-          onPressed: () => Navigator.pop(context),
-        ),
+      body: const FormularioDivision(),
+    );
+  }
+}
+
+class FormularioDivision extends StatefulWidget {
+  const FormularioDivision({Key? key}) : super(key: key);
+
+  @override
+  FormularioDivisionState createState() => FormularioDivisionState();
+}
+
+class FormularioDivisionState extends State<FormularioDivision> {
+  final _formDivisionKey = GlobalKey<FormState>();
+
+  final _numeroUnoController = TextEditingController();
+  final _numeroDosController = TextEditingController();
+
+  double _resultado = 0;
+  int _uno = 0, _dos = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Form(
+      key: _formDivisionKey,
+      child: Column(
+        children: <Widget>[
+          TextFormField(
+            keyboardType: TextInputType.number,
+            controller: _numeroUnoController,
+            decoration:
+                const InputDecoration(labelText: 'Entre el primer número'),
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'Ingresa un valor';
+              }
+            },
+          ),
+          TextFormField(
+            keyboardType: TextInputType.number,
+            controller: _numeroDosController,
+            decoration:
+                const InputDecoration(labelText: 'Entre el segundo número'),
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'Ingresa un valor';
+              }
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                MaterialButton(
+                  onPressed: () {
+                    if (_formDivisionKey.currentState!.validate()) {
+                      _uno = int.parse(_numeroUnoController.text);
+                      _dos = int.parse(_numeroDosController.text);
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text("la division es ${_uno + _dos}"),
+                        ),
+                      );
+                    }
+                    setState(() {
+                      _resultado = _uno / _dos;
+                    });
+                  },
+                  child: const Text('Division'),
+                  color: Colors.orangeAccent,
+                ),
+                MaterialButton(
+                  child: const Text('Regresa papu'),
+                  color: Colors.redAccent,
+                  onPressed: () => Navigator.pop(context),
+                ),
+              ],
+            ),
+          ),
+          const Text(
+            "Resultado:",
+            style: TextStyle(fontSize: 32.0),
+          ),
+          Text(
+            _resultado.toString(),
+            style: const TextStyle(fontSize: 40.0),
+          ),
+        ],
       ),
     );
   }
--- lib/screens/multiplicacion.dart
+++ lib/screens/multiplicacion.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_field
+
 import 'package:flutter/material.dart';
 
 class Multiplicacion extends StatelessWidget {
@@ -6,16 +8,100 @@ class Multiplicacion extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+    const appTitulo = "Multiplicación";
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Multipliación'),
+        title: const Text(appTitulo),
       ),
-      body: Center(
-        child: MaterialButton(
-          child: const Text('Regresar'),
-          color: Colors.orangeAccent,
-          onPressed: () => Navigator.pop(context),
-        ),
+      body: const FormularioMultiplicacion(),
+    );
+  }
+}
+
+class FormularioMultiplicacion extends StatefulWidget {
+  const FormularioMultiplicacion({Key? key}) : super(key: key);
+
+  @override
+  FormularioMultiplicacionState createState() =>
+      FormularioMultiplicacionState();
+}
+
+class FormularioMultiplicacionState extends State<FormularioMultiplicacion> {
+  final _formMultiplicacionKey = GlobalKey<FormState>();
+
+  final _numeroUnoController = TextEditingController();
+  final _numeroDosController = TextEditingController();
+
+  int _resultado = 0;
+  int _uno = 0, _dos = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Form(
+      key: _formMultiplicacionKey,
+      child: Column(
+        children: <Widget>[
+          TextFormField(
+            keyboardType: TextInputType.number,
+            controller: _numeroUnoController,
+            decoration:
+                const InputDecoration(labelText: 'Digite el primer número'),
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'Ingrese un valor';
+              }
+            },
+          ),
+          TextFormField(
+            keyboardType: TextInputType.number,
+            controller: _numeroDosController,
+            decoration:
+                const InputDecoration(labelText: 'Digite el segundo número'),
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'Ingrese un valor';
+              }
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                MaterialButton(
+                  child: const Text('Multiplicar'),
+                  color: Colors.orangeAccent,
+                  onPressed: () {
+                    if (_formMultiplicacionKey.currentState!.validate()) {
+                      _uno = int.parse(_numeroUnoController.text);
+                      _dos = int.parse(_numeroDosController.text);
+                    }
+                    setState(() {
+                      _resultado = _uno * _dos;
+                    });
+                  },
+                ),
+                MaterialButton(
+                  child: const Text('Regresar'),
+                  color: Colors.redAccent,
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+            ),
+          ),
+          const Text(
+            'Resultado',
+            style: TextStyle(fontSize: 32.0),
+          ),
+          Text(
+            _resultado.toString(),
+            style: const TextStyle(fontSize: 40.0),
+          ),
+        ],
       ),
     );
   }
